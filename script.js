198ff6e39749b3be23ed383ae147c1c231a31739
petrovicmarko <markopetrovic0503@gmail.com>
2022-08-18 16:16:00 +0200

    x o with change added

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ gameBtn.addEventListener("click", () => {
     header.style.display = "none"
     field.style.display = "grid"
 })
+
+let state = 0;
+
+const slotsColl = document.getElementsByClassName("slot");
+const slotsArr = Array.from(slotsColl);
+
+slotsArr.forEach(slot => {
+    const slotsText = Array.from(slot.children)
+    slot.addEventListener("click", () => {
+
+        if (state % 2 == 0) {
+            slotsText[0].textContent = "X"
+        } else {
+            slotsText[0].textContent = "O"
+        }
+
+        slotsText[0].style.opacity = "0"
+
+        state++
+    })
+});
