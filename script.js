e06730823b4191265b25232fb5ae0f006183fc4b
petrovicmarko <markopetrovic0503@gmail.com>
2022-08-18 14:40:17 +0200

    field with basic styles added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gameBtn = document.getElementById("play")
 const header = document.getElementById("header")
+const field = document.getElementById("field")
 
 gameBtn.addEventListener("click", () => {
-    gameBtn.style.display = "none";
-    header.style.display = "none";
+    gameBtn.style.display = "none"
+    header.style.display = "none"
+    field.style.display = "grid"
 })
